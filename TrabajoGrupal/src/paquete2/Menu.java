4adc1acbd58a856feddc9e6fd6eac6f9410e166d
alexander-jimenez11 <gajimenez2@utpl.edu.ec>
2021-06-24 16:10:33 -0500

    Tarea Grupal

---
+++ TrabajoGrupal/src/paquete2/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+/**
+ *
+ * @author Alexander
+ */
+public abstract class Menu {
+    
+
+protected String nombrePlato;
+protected double valorMenu;
+protected double valorInicialMenu;
+
+public Menu (String nompla, double valmenu, double valinimenu) 
+        ´
+}
+
